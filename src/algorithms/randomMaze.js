ef024183df8e66345e4c83c8c501e0ca49d69dd6
fawzeus <rjilifawzi@gmail.com>
2023-11-19 03:05:00 +0100

    fix random maze

--- src/algorithms/randomMaze.js
+++ src/algorithms/randomMaze.js
@@ -6,7 +6,7 @@ export function createRandomMaze(grid, rows, columns) {
   }
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
-      grid[i][j].isWall = Math.random() > 0.5 ? true : false;
+      grid[i][j].isWall = Math.random() < 0.3 ? true : false;
     }
   }
 }
